3671afea7e6f45f1300e7288d7808cf185979b51
Faiso Abdirisak <91128983+faisoabdirisak@users.noreply.github.com>
2024-01-27 11:30:57 +0300

    :tada: React redux demo

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux'
+import store from './redux/Store'
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
